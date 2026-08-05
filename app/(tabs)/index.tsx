35c162f06248c071b7e476ced98fa66aaadd8404
sarabi <sarabeytuie@gmail.com>
2026-08-05 17:37:28 +0330

    feat: add local images for each product

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
 import Header from "@/components/Header";
 import SafeAreaView from "@/components/SafeAreaView";
+import { productImages } from "@/lib/images";
 import { useGetProductsQuery } from "@/store/api/productApi";
-import { FlatList, Text, View } from "react-native";
+import { router } from "expo-router";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 
 const Home = () => {
   const { data: products } = useGetProductsQuery();
@@ -16,14 +18,22 @@ const Home = () => {
           columnWrapperStyle={{ justifyContent: "space-between" }}
           renderItem={({ item }) => (
             <View className="w-[48%] mb-10 gap-4">
-              <View className="bg-vistas-white rounded-lg shadow-md p-4">
-                <View className="h-40 bg-gray-200 rounded-lg mb-4" />
-                <View>
-                  <Text className="text-lg font-semibold text-cod-gray">
-                    {item.name}
-                  </Text>
-                  <Text className="text-sm text-scorpion">{item.price}</Text>
-                </View>
+              <Pressable onPress={() => router.push(`/product/${item.id}`)}>
+                <Image
+                  source={(productImages as any)[item.thumbnail]}
+                  className="w-full h-62 rounded-2xl"
+                />
+              </Pressable>
+              <View className="gap-1.5">
+                <Text className="text-base font-medium tracking-widest text-scorpion uppercase">
+                  {item.category}
+                </Text>
+                <Text className="text-lg font-medium leading-tight text-cod-gray">
+                  {item.name}
+                </Text>
+                <Text className="text-base font-bold text-cobalt uppercase">
+                  ${item.price}
+                </Text>
               </View>
             </View>
           )}
---
+++ lib/images.ts
@@ -0,0 +1,8 @@
+export const productImages = {
+  headphone: require("../assets/images/products/headphone.jpeg"),
+  watch: require("../assets/images/products/watch.jpeg"),
+  shoes: require("../assets/images/products/shoes.jpeg"),
+  backpack: require("../assets/images/products/backpack.webp"),
+  coffee: require("../assets/images/products/coffee.jpg"),
+  lamp: require("../assets/images/products/lamp.jpg"),
+};
